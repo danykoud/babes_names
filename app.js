48485767df3e22e6d8e343ce1cd2d412f4572db6
Dany Koudede <koudededany@icloud.com>
2020-09-18 10:31:56 -0700

    second commit

--- app.js
+++ app.js
@@ -1,77 +1,48 @@
 // from data.js
 // function buildtable() {
-  
- d3.csv("ALL_STATES.csv", function(data) {
- 
 
-console.log(data);
+d3.csv("../Resources/Names.csv", function (data) {
 
-var tableData= data
-// get table references
-var tbody = d3.select("tbody");
-
-function buildTable(data) {
-  // First, clear out any existing data
-  tbody.html("");
-
-  // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  Object.values(data).forEach((dataRow) => {
-    // Append a row to the table body
-    var row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      var cell = row.append("td");
-      cell.text(val);
+  console.log(data);
+  var tableData = data
+  // get table references
+  var tbody = d3.select("tbody");
+  function buildTable(data) {
+    tbody.html("");
+    Object.values(data).forEach((dataRow) => {
+      // Append a row to the table bodys
+      var row = tbody.append("tr");
+      // Loop through each field in the dataRow and add
+      Object.values(dataRow).forEach((val) => {
+        var cell = row.append("td");
+        cell.text(val);
+      });
     });
-  });
-}
-
- 
-// Keep Track of all filters
-var filters = {};
-
-function updateFilters() {
-
-  // Save the element, value, and id of the filter that was changed
-  var changedElement = d3.select(this).select("input");
-  var elementValue = changedElement.property("value");
-  var filterId = changedElement.attr("id");
-
-  // If a filter value was entered then add that filterId and value
-  // to the filters list. Otherwise, clear that filter from the filters object
-  if (elementValue) {
-    filters[filterId] = elementValue;
   }
-  else {
-    delete filters[filterId];
+  var filters = {};
+  function updateFilters() {
+    // Save the element, value, and id of the filter that was changed
+    var changedElement = d3.select(this).select("input");
+    var elementValue = changedElement.property("value");
+    var filterId = changedElement.attr("id");
+    if (elementValue) {
+      filters[filterId] = elementValue;
+    }
+    else {
+      delete filters[filterId];
+    }
+    // Call function to apply all filters and rebuild the table
+    // filterTable();
+     // Set the filteredData to the tableData
+     let filteredData = tableData;
+     Object.entries(filters).forEach(([key, value]) => {
+       filteredData = filteredData.filter(row => row[key] === value);
+     });
+     // Finally, rebuild the table using the filtered Data
+     buildTable(filteredData);
   }
-
-  // Call function to apply all filters and rebuild the table
-  filterTable();
-
-}
-
-function filterTable() {
-
-  // Set the filteredData to the tableData
-  let filteredData = tableData;
-
-  // Loop through all of the filters and keep any data that
-  // matches the filter values
-  Object.entries(filters).forEach(([key, value]) => {
-    filteredData = filteredData.filter(row => row[key] === value);
-  });
-
-  // Finally, rebuild the table using the filtered Data
-  buildTable(filteredData);
-}
-
-// Attach an event to listen for changes to each filter
-d3.selectAll(".filter").on("change", updateFilters);
-
-// Build the table when the page loads
-buildTable(tableData);
+  
+  d3.selectAll(".filter").on("change", updateFilters);
+  // Build the table when the page loads
+  buildTable(tableData);
 })
--- app.py
+++ app.py
@@ -1,69 +1,147 @@
-# import necessary libraries
-from models import create_classes
-import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+from flask import Flask, jsonify, render_template
 
+#################################################
 # Flask Setup
+#################################################
 app = Flask(__name__)
 
 
-# baby_names= base.classes.baby_names
+#################################################
+# Flask Routes
+#################################################
 
-from flask_sqlalchemy import SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:Koudede$89@localhost:5432/babynames_db"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# reflect the existing base into a new model 
-base=automap_base()
-#reflect the table 
-base.prepare(engine, reflect=True)
-#save reference to the table 
-baby_names= create_classes(db)
-
-
-# create route that renders index.html template
 @app.route("/")
-def home():
-    return render_template("index.html")
+def welcome():
+    return (
+        f"<h4>Available Routes:</h4>"
+        f'<a href="/api/v1.0/ids">/api/v1.0/ids</a><br/>'
+        f'<a href="/api/v1.0/info/1286">/api/v1.0/info/subject_id</a><br/>'
+        f'<a href="/api/v1.0/subjects">/api/v1.0/subjects</a><br/>'
+        f'<a href="/api/v1.0/subjects/1286">/api/v1.0/subjects/subject_id</a><br/>'
+        f'<a href="/"><h4>Back</h4></a><br/>'
+    )
 
-@app.route("/api/map")
-def data():
 
-return render_template("map.html")
 
-@app.route("/api/graph ")
-def data():
-    results = db.session.query(names.Name, names.Number).all()
 
-    hover_text = [result[0] for result in results]
-    Number = [result[1] for result in results]
-    year = [result[2] for result in results]
-    names_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "Number": Number,
-        "year": year,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
 
-    return jsonify(Names_data)
-if __name__ == "__main__":
-    app.debug =True
-    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import necessary libraries
+
+# import os
+# from flask import (
+#     Flask,
+#     render_template,
+#     jsonify,
+#     request,
+#     redirect)
+# from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+# # Flask Setup
+# app = Flask(__name__)
+# @app.route("/")
+# def home():
+#     return ("hello word")
+# if __name__ == "__main__":
+#     app.run()
+
+# ENV = 'prod'
+
+# if ENV == 'dev':
+
+#     app.debug = True
+
+#     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:Koudede$89@localhost:5432/babynames_db"
+#     # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///babynames_db"
+# else:
+#     app.debug = False
+#     app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    
+# # Remove tracking modifications
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+# reflect the existing base into a new model 
+# def create_classes(db):
+#     class names(db.Model):
+#         __tablename__ = 'baby_names'
+        
+#         id = db.Column(db.Integer, primary_key=True)
+#         state = db.Column(db.String(64))
+#         sex = db.Column(db.String(10))
+#         year = db.Column(db.Float)
+#         name = db.Column(db.String(64))
+#         number = db.Column(db.Float)
+        
+#         def __init__(self,state,sex,year, name,number):
+
+#             self.state = state
+#             self.sex = sex
+#             self.year = year
+#             self.name = name
+#             self.number = number
+       
+#         def __repr__(self):
+#             return '<names %r>' % (self.name)
+    
+
+
+
+
+# names = create_classes(db)
+
+
+# # create route that renders index.html template
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/Data")
+# def home():
+#     return render_template("data.html")
+
+# @app.route("/data/name")
+# def data():
+#     results = db.session.query(names.name, names.number, names.sex, names.state).all()
+
+
+#     names_array = []
+#     for result in results:
+
+#         c = {}
+
+#         c["name"] = result[0]
+#         c["number"] = result[1]
+#         c["sex"] = result[2]
+#         c["state"] = result[3]
+
+#         names_array.append(c)
+#     return jsonify(names_array)
+    
+# if __name__ == "__main__":
+#     app.run()
 
