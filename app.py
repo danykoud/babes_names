7c729a6e37da4c53f67655abe766200a46b26385
Dany Koudede <koudededany@icloud.com>
2020-09-21 07:29:21 -0700

    python work
    change from 3  py files to one
    imported json
    changed to sqlalchemy (app)  init
    created a model table
    tested app
    worked with data
    debug

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from config import database, connect_string
-from BabiesNamesData import BabiesNamesData
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, request, jsonify, redirect
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect,join
+from flask import (Flask, render_template, jsonify, request, redirect)
+from flask_sqlalchemy import SQLAlchemy
+import json
+import collections
 
 ######## INITIATE FLASK APP #########################
 app= Flask(__name__)
-app.static_folder = 'static'
 
-db= BabiesNamesData()
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://uojmftnmharlnr:2afb0b29037d9b4cbf71fe7772cb4a0f499dad2c2133f74579a1f5d7cd3726da@ec2-54-160-202-3.compute-1.amazonaws.com:5432/d1rehkgcfdf1oa'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class baby_names(db.Model):
+    __tablename__ = 'baby_names'
+    id = db.Column(db.Integer, primary_key=True)
+    State= db.Column(db.String(200))
+    Sex = db.Column(db.String(20))
+    Year = db.Column(db.Integer)
+    Name = db.Column(db.String(200))
+    Number = db.Column(db.Integer)
+    Latitude = db.Column(db.Float)
+    Longitude = db.Column(db.Float)
+    City = db.Column(db.String(200))
+
+    def __init__(self, State, Sex, Year, Name, Latitude, Longitude,City):
+        self.State = State
+        self.Sex =Sex
+        self.Year = Year
+        self.Name = Name
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+    
+    def __repr__(self):
+
+        return '<babynames_us %r>' % (self.Name)
 #################################################
 # Flask Routes
 #################################################
@@ -22,11 +56,27 @@ def show_apis():
         f"<h4>Available Routes:</h4>"
         f'<a href="/api/v1.0/Data">/api/v1.0/Data</a><br/>'        
     )    
-@app.route("/api/v1.0/Data")
-def get_data():
-    
-    return jsonify(db.all_data)
+@app.route("/api/v1.0/Data", methods=["GET", 'POST'])
+def all_data():
+    sel = [baby_names.Name, baby_names.State,baby_names.Sex, baby_names.Number,baby_names.Year, baby_names.Latitude,baby_names.Longitude,baby_names.City]
+    results= db.session.query(*sel).filter(baby_names.Year >=1980 ).filter(baby_names.Year <=2018)
+    all_data=[]
+    for result in results:
+        d = collections.OrderedDict()
+        d["Name"]=result[0]
+        d["State"]=result[1]
+        d["Sex"]=result[2]
+        d["Number"]=result[3]
+        d["Year"]=result[4]
+        d["Latitude"]=result[5]
+        d["Longitude"]=result[6]
+        d["City"]=result[7]
+        # r= result[0] + result[0] + result[0] + result[0] + result[0] + result[0] + result[0] + result[0]
+        all_data.append(d)
+        json.dumps(all_data)
+        # with open("static/js/covid_data.js", "w") as f:
+        #     f.write(covid_json)
+    return jsonify('all_data')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
